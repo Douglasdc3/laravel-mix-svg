fd520954ba684b1cf15dc3ab0160a85b611ef3f7
Kyle Milloy <kmilloy@nubix.ca>
2019-09-05 13:50:05 -0600

    add ability to add svgs recursively

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-'use strict';
+"use strict";
 
-let mix = require('laravel-mix');
-let fs = require('fs');
+let mix = require("laravel-mix");
+let fs = require("fs");
 
 function writeJavascript(file, icons, defaultClass) {
-  fs.writeFileSync(file, `
+  fs.writeFileSync(
+    file,
+    `
     const icons = {
       ${icons}
     }
@@ -19,27 +21,31 @@ function writeJavascript(file, icons, defaultClass) {
 
       return icons[name].replace('<svg ', '<svg class="' + globalClass + ' ' + classes + '"');
     };
-  `);
+  `
+  );
 }
 
 class SVG {
   register(options) {
-    this.options = Object.assign({
-      class: '',
-      assets: ['./resources/svg/'],
-      output: './resources/js/svg.js',
-    }, options);
+    this.options = Object.assign(
+      {
+        class: "",
+        assets: ["./resources/svg/"],
+        output: "./resources/js/svg.js",
+      },
+      options
+    );
   }
 
   boot() {
-    let icons = '';
+    let icons = "";
 
-    this.options.assets.forEach((path) => {
-      fs.readdirSync(path).forEach(file => {
-        if (!file.endsWith('.svg')) return;
+    this.options.assets.forEach(path => {
+      let files = this.walker(path);
 
-        const iconName = file.replace('.svg', '');
-        const icon = (fs.readFileSync(path + file) + '');
+      files.forEach(file => {
+        const iconName = file.replace(".svg", "");
+        const icon = fs.readFileSync(path + file) + "";
 
         icons += `'${iconName}': \`${icon}\`,\n\r`;
       });
@@ -47,6 +53,24 @@ class SVG {
 
     writeJavascript(this.options.output, icons, this.options.class);
   }
+
+  walker(dir, filelist = [], prefix = "") {
+    let files = fs.readdirSync(dir);
+
+    files.forEach(file => {
+      if (fs.statSync(dir + file).isDirectory()) {
+        let prefix = file + "/";
+
+        filelist = this.walker(dir + prefix, filelist, prefix);
+      } else {
+        if (!file.endsWith("svg")) return;
+
+        filelist.push(prefix + file);
+      }
+    });
+
+    return filelist;
+  }
 }
 
-mix.extend('svg', new SVG());
+mix.extend("svg", new SVG());
